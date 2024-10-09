0b3df11aa427e30b7de66d1b085963a699ca8ab1
Gimpeh <thatgimpehfellow@gmail.com>
2024-10-08 22:57:55 -0500

    setup

---
+++ QFT/setup.lua
@@ -0,0 +1,3 @@
+os.execute("mkdir /usr/bin")
+os.execute("rm /usr/bin/qft_manager.lua")
+os.execute("wget https://raw.githubusercontent.com/Gimpeh/gimptopia/refs/heads/main/QFT/qft_manager.lua /usr/bin/qft_manager.lua")
